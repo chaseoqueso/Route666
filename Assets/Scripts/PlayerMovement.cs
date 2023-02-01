2642ec16dae0c720e82ab0479af864dcb90a8094
chaseoqueso <chaseoqueso@gmail.com>
2023-02-01 14:48:19 -0800

    Drifting is bad

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using static UnityEngine.InputSystem.InputAction;
@@ -7,9 +8,18 @@ using static UnityEngine.InputSystem.InputAction;
 [RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
 public class PlayerMovement : MonoBehaviour
 {
+    public enum DriftState
+    {
+        None = 0,
+        Left = -1,
+        Right = 1,
+    }
+
     [Header("References")]
     [Tooltip("The root object of the player model.")]
     [SerializeField] private GameObject model;
+    [Tooltip("The CinemachineFreeLook script driving the camera.")]
+    [SerializeField] private CinemachineFreeLook freeLook;
 
     [Header("Acceleration")]
     [Tooltip("The rate of acceleration in units/s^2.")]
@@ -22,21 +32,32 @@ public class PlayerMovement : MonoBehaviour
     [Header("Turning")]
     [Tooltip("The maximum change of direction in degrees/s.")]
     [SerializeField] private float maxTurnAngle;
-    [Tooltip("The maximum speed the player approaches the maxTurnAngle in degrees/s^2.")]
+    [Tooltip("The speed at which the player approaches the maxTurnAngle in degrees/s^2.")]
     [SerializeField] private float maxTurnChange;
     [Tooltip("The maximum angle the model will lean.")]
     [SerializeField] private float maxLeanAngle;
+
+    [Header("Drifting")]
+    [Tooltip("The range above and below maxTurnAngle that turning inputs will range within while drifting.")]
+    [SerializeField] private float driftAngleRange;
+    [Tooltip("The angle to skew the model while drifting.")]
+    [SerializeField] private float driftModelSkewAngle;
+    [Tooltip("The range above and below the skew angle to skew the model while drifting.")]
+    [SerializeField] private float driftModelSkewRange;
+    [Tooltip("The speed at which the player model approaches the drift angle in degrees/s^2.")]
+    [SerializeField] private float maxSkewChange;
 
     // Input
     private PlayerInput input;  // The PlayerInput attached to this GameObject
     private float driveInput;   // The current input value for accelerating or braking
     private float turnInput;    // The current input value for turning
-    private float driftInput;   // The current input value for drifting
 
     // Motion
     private CharacterController controller;     // The CharacterController that handles player movement
+    private DriftState driftState;              // Whether the player is in a drift or not, and if so which direction
     private float velocity;                     // The current forward velocity
     private float turnAngle;                    // The current change in direction
+    private float driftSkewAngle;               // The current drifting skew angle for the model
 
     void Start()
     {
@@ -72,12 +93,55 @@ public class PlayerMovement : MonoBehaviour
         // --- TURNING ---
         // ---------------
 
-        // Apply turning input
-        turnAngle = Mathf.MoveTowards(turnAngle, turnInput * maxTurnAngle, maxTurnChange * Time.fixedDeltaTime);            // Apply the turn input to the turn angle
-        transform.Rotate(0, turnAngle * Time.fixedDeltaTime, 0);                                                            // Rotate the player around the Y-axis
-        model.transform.localRotation = Quaternion.AngleAxis((turnAngle/maxTurnAngle) * -maxLeanAngle, Vector3.forward);    // Lean the model around the Z-axis
+        // Calculate the min and max angle the player can turn
+        Vector2 angleRange;
+        if(driftState == DriftState.None)   // If we aren't currently drifting
+        {
+            angleRange = new Vector2(-maxTurnAngle, maxTurnAngle);      // The angle range is the full turn range
+        }
+        else    // If we are drifting
+        {
+            float driftAngle = (maxTurnAngle + driftAngleRange) * (int)driftState;                  // Calculate the center of the angle range
+            angleRange = new Vector2(driftAngle - driftAngleRange, driftAngle + driftAngleRange);   // The angle range is a narrow range beyond the max turn angle
+        }
 
-        controller.Move(transform.forward * velocity * Time.fixedDeltaTime);        // Move the player
+        // Calculate ideal angle
+        float normalizedTurnInput = Mathf.InverseLerp(-1, 1, turnInput);                            // Get the turnInput from a scale of 0 to 1
+        float idealTurnAngle = Mathf.SmoothStep(angleRange.x, angleRange.y, normalizedTurnInput);   // Get the desired turn angle based on the current angle range
+
+        // Turn towards ideal angle
+        turnAngle = Mathf.MoveTowards(turnAngle, idealTurnAngle, maxTurnChange * Time.fixedDeltaTime);      // Apply the turn input to the turn angle
+        transform.Rotate(0, turnAngle * Time.fixedDeltaTime, 0);                                            // Rotate the player around the Y-axis
+
+        // Move the player
+        controller.Move(transform.forward * velocity * Time.fixedDeltaTime);
+        
+            
+        // ----------------------
+        // --- MODEL ROTATION ---
+        // ----------------------
+
+        // Lean the model around the Z-axis
+        float leanAngle = turnAngle/maxTurnAngle * -maxLeanAngle;                           // Calculate the angle to lean the player model
+        Quaternion leanRotation = Quaternion.AngleAxis(leanAngle, Vector3.forward);         // Set the lean rotation
+
+        // Skew the model if drifting
+        float idealSkewAngle;
+        if(driftState != DriftState.None)   // If drifting
+        {
+            float skewAngle = driftModelSkewAngle * (int)driftState;        // The middle of the skew angle range
+            Vector2 skewRange = new Vector2(skewAngle - driftModelSkewRange, skewAngle + driftModelSkewRange);  // The angle range to skew the model
+            idealSkewAngle = Mathf.SmoothStep(skewRange.x, skewRange.y, normalizedTurnInput);                   // Get the desired skew angle based on the skew angle range
+        }
+        else    // If not drifting
+        {
+            idealSkewAngle = 0;
+        }
+        driftSkewAngle = Mathf.MoveTowards(driftSkewAngle, idealSkewAngle, maxSkewChange * Time.fixedDeltaTime);    // Move the drift angle toward the ideal
+        Quaternion skewRotation = Quaternion.AngleAxis(driftSkewAngle, Vector3.up);                                 // Set the skew rotation
+
+        // Set the model rotation
+        model.transform.localRotation = skewRotation * leanRotation;
     } 
 
     // This gets called whenever there is a change in the input for the Drive action
@@ -95,6 +159,33 @@ public class PlayerMovement : MonoBehaviour
     // This gets called whenever there is a change in the input for the Drift action
     public void OnDrift(CallbackContext context)
     {
-        driftInput = context.ReadValue<float>();
+        // ----------------
+        // --- DRIFTING ---
+        // ----------------
+
+        float driftInput = context.ReadValue<float>();  // Read the input value
+        if(driftState == DriftState.None)               // If not currently drifting
+        {
+            if(driftInput > 0)                          // If drift input is active
+            {
+                if(Mathf.Abs(turnInput) > 0)            // If the player is turning, set the drift state based on turn direction
+                {
+                    driftState = (DriftState)Mathf.Sign(turnInput);
+                }
+                else                                    // If the player is not turning, set the drift state based on look direction
+                {
+                    driftState = (DriftState)Mathf.Sign(freeLook.m_XAxis.Value + 0.000001f);
+                }
+            }
+        }
+        else                        // If currently drifting
+        {
+            if(driftInput == 0)     // If drift input is released, stop drifting
+            {
+                driftState = DriftState.None;
+            }
+        }
+
+        Debug.Log(driftState);
     }
 }
