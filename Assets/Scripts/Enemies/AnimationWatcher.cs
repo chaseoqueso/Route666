287c13e7068c848de30901374880a00e19ae37fd
chaseoqueso <chaseoqueso@gmail.com>
2023-02-03 23:33:06 -0800

    Enemy model & animation start

---
+++ Assets/Scripts/Enemies/AnimationWatcher.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimationWatcher : MonoBehaviour
+{
+    public UnityEvent onAnimationTrigger;
+
+    public void triggerEvents()
+    {
+        
+    }
+}
